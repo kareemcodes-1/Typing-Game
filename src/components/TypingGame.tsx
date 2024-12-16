e232bc9492454c66d07cfe9c68a42217cb996a4b
Kareem Braimoh <kareembrai222@gmail.com>
2024-12-16 13:35:50 +0100

    small updates

--- src/components/TypingGame.tsx
+++ src/components/TypingGame.tsx
@@ -3,36 +3,46 @@ import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 const TypingGame = () => {
 
     const data = 'The bright morning sun rose over the tranquil ocean, painting the sky in hues of orange and pink, as waves gently kissed the shore, whispering secrets of the deep, endless horizon The bright morning sun rose over the tranquil ocean, painting the sky in hues of orange and pink, as waves gently kissed the shore, whispering secrets of the deep, endless horizon The bright morning sun rose over the tranquil ocean, painting the sky in hues of orange and pink, as waves gently kissed the shore, whispering secrets of the deep, endless horizon';
+
+    useEffect(() => {
+      ref.current?.focus();
+    }, [])
     
 
-    const [inputValue, setInputValue] = useState<string[]>(data.split(''));
+    const ref = useRef<HTMLTextAreaElement | null>(null);
+    const [inputValue] = useState<string[]>(data.split(''));
     const [active, setActive] = useState<boolean[]>([]);
     const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [errorCount, setErrorCount] = useState<number>(0);
     const [score, setScore] = useState<number>(0);
     const [endGame, setEndGame] = useState<boolean>(false);
 
-    const [timer, setTimer] = useState<number>(60);
+    const [timer, setTimer] = useState<number>(30);
+    const [hasRun, setHasRun] = useState(false);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTimer((prevTimer) => {
-                if(prevTimer <= 1){
-                    clearInterval(interval);
-                    setEndGame(true);
-                    return 0;
-                }else{
-                    return prevTimer - 1;
-                }
-            })
-        }, 1000);
+    function runTimer(){
+      const interval = setInterval(() => {
+        setTimer((prevTimer) => {
+            if(prevTimer <= 1){
+                clearInterval(interval);
+                setEndGame(true);
+                return 0;
+            }else{
+                return prevTimer - 1;
+            }
+        });
+    }, 1000);
 
-        return () => {
-            clearInterval(interval);
-        }
-    }, []);
+    return interval;
+    }
 
 
     function handleChange(e: ChangeEvent<HTMLTextAreaElement>){
+        if(hasRun === false){
+          runTimer();
+        }
+        setHasRun(true);
+
         const typedChar = e.target.value; 
         e.target.value = '';
 
@@ -45,18 +55,16 @@ const TypingGame = () => {
         }else{
             const value = false;
             setActive([...active, value]);
+            setErrorCount((errorCount) => errorCount + 1);
         }
     }
 
-    console.log(active);
-
-
-
-
+    // console.log(currentIndex);
+    // console.log(active[0]);
 
   return (
-    <div className='flex items-start justify-center flex-col'>
-        {!endGame && <h2 className='text-[2.5rem] text-white'>{timer}</h2>}
+    <div className='flex items-start justify-center flex-col p-[1rem]'>
+        {!endGame && <h2 className='text-[2.5rem] text-white'>⏱ {timer}</h2>}
         <div className={`flex items-center justify-center ${endGame ? 'h-[100vh]' : 'h-[60vh]'}  w-full`}>
             {endGame ? 
             <>
@@ -73,16 +81,16 @@ const TypingGame = () => {
             </div>
             }
             </>
-             : <div className="h-[20vh] w-[70%] relative" id="scroll-container">
+             : <div className="h-[29vh] w-[75%] relative" id="scroll-container">
              <div
-               className="absolute top-0 left-0 w-full h-full overflow-y-scroll no-scrollbar text-white text-[2rem] pointer-events-none"
+               className="absolute top-0 left-0 w-full h-full overflow-y-scroll no-scrollbar text-white text-[2.5rem] pointer-events-none"
                style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}
              >
                {inputValue.map((value, index) => (
                  <span
                    key={index}
                    className={`${
-                    active[index] === true ? 'opacity-100 text-white' : active[index] === false ? 'opacity-100 text-red-500' : 'opacity-15'
+                    active[index] === true ? 'opacity-100 transition-opacity text-white' : active[index] === false ? 'opacity-100 text-red-500 transition-opacity' : 'opacity-15'
                   }`}
                  >
                    {value}
@@ -90,7 +98,7 @@ const TypingGame = () => {
                ))}
              </div>
            
-             <textarea
+             <textarea ref={ref}
                id="words-input"
                className="absolute top-0 left-0 w-full h-full bg-transparent text-transparent outline-none border-0 resize-none"
                onChange={(e) => {
@@ -106,6 +114,16 @@ const TypingGame = () => {
            
         }
       </div>
+
+      {!endGame && (
+        <div className='flex items-end justify-end text-end ml-auto pr-[2rem]'>
+        <div className='flex items-center gap-[1rem]'>
+        <h2 className='text-[2rem] text-white flex'>😑 Errors:&nbsp;<span className='text-red-500'>{errorCount}</span></h2> 
+        <span className='text-[2rem] text-white'>|</span>
+        <h2 className='text-[2rem] text-white flex'>👀 WPM: {currentIndex}</h2>
+        </div>
+      </div>
+      )}
     </div>
   )
 }
